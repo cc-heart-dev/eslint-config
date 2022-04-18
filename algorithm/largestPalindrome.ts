9cddb3052a3751ddac605d80ae6c2b6d4fb57c11
xchen <7362469@qq.com>
2022-04-18 23:29:45 +0800

    feat: add lexicalOrder

---
+++ algorithm/largestPalindrome.ts
@@ -0,0 +1,12 @@
+/**
+ * @author heart
+ * @description
+ * @Date 2022-04-16
+ * @description: 同时由于两个 nn 位整数的乘积至多是个 2n2n 位数 未完成
+ */
+
+function largestPalindrome(n: number): number {
+  let ans: number
+
+  return ans
+}
---
+++ test/algorithm/lexicalOrder.test.ts
@@ -0,0 +1,9 @@
+import { lexicalOrder, lexicalOrder1 } from '../../algorithm/lexicalOrder'
+
+describe('test array is lexicalOrder', () => {
+  it('is equal', () => {
+    for (let i = 1; i < 13; i++) {
+      expect(lexicalOrder(i)).toEqual(expect.arrayContaining(lexicalOrder1(i)))
+    }
+  })
+})
