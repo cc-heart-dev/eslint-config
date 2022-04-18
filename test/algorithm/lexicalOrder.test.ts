47ac8751bb22cb3e5236963c50547fb742744a2b
xchen <7362469@qq.com>
2022-04-18 23:31:13 +0800

    add test docs

--- test/algorithm/lexicalOrder.test.ts
+++ test/algorithm/lexicalOrder.test.ts
@@ -4,6 +4,7 @@ describe('test array is lexicalOrder', () => {
   it('is equal', () => {
     for (let i = 1; i < 13; i++) {
       expect(lexicalOrder(i)).toEqual(expect.arrayContaining(lexicalOrder1(i)))
+      expect(lexicalOrder1(i)).toEqual(expect.arrayContaining(lexicalOrder(i)))
     }
   })
 })
