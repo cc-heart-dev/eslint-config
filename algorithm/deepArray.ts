e3720d38c1a2bc9028b8b17471b815ea6036580b
xchen <7362469@qq.com>
2022-04-24 18:30:01 +0800

    feat:add deepArray

---
+++ algorithm/deepArray.ts
@@ -0,0 +1,18 @@
+ // 层级嵌套数组
+const num = 3;
+
+let arr = new Array(num).fill(0);
+const heart = arr
+
+function deepArray(len,lastLen,arr) {
+  if (len >= lastLen) return;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(num).fill(0);
+    deepArray(len + 1, lastLen,arr[i])
+  }
+}
+
+// 差值 + 1 为当前层级
+deepArray(1, 3,arr)
+
+console.log(heart);
