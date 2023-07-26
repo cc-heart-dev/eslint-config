b22e5b236b0bff7045449b8a6c0651d83f11239e
heart <7362469@qq.com>
2023-07-26 10:48:55 +0800

    chore: update config

--- vue-ts-eslint.js
+++ vue-ts-eslint.js
@@ -1,7 +1,8 @@
-const { ignorePatterns, env } = require('./config')
+const { ignorePatterns, env } = require("./config");
 
 module.exports = {
   env,
+  // npm install eslint-plugin-vue
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "vue-eslint-parser",
   plugins: ["@typescript-eslint"],
@@ -11,8 +12,8 @@ module.exports = {
   },
   ignorePatterns,
   rules: {
-    '@typescript-eslint/no-explicit-any': 0,
+    "@typescript-eslint/no-explicit-any": 0,
     "no-undef": "off", // supports unplugin-auto-imports
-    '@typescript-eslint/consistent-type-imports': 'error', // 当只引用类型时，自动加上 type 操作符
+    "@typescript-eslint/consistent-type-imports": "error", // 当只引用类型时，自动加上 type 操作符
   },
 };
